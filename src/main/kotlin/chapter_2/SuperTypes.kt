01a27dbb37477c16451d3458a8bcae3799f975db
VitasSalvantes <salvantesvitas@gmail.com>
2021-07-28 17:22:40 +0200

    I learned basic types in Kotlin

---
+++ src/main/kotlin/chapter_2/SuperTypes.kt
@@ -0,0 +1,34 @@
+package chapter_2
+
+fun main() {
+    val typeByte: Byte = 1
+    val typeShort: Short = 12
+    val typeInt: Int = 123
+    val typeLong: Long = 1234
+    val typeFloat: Float = 1.2F
+    val typeDouble: Double = 1.23
+    val typeBoolean: Boolean = true
+    val typeChar: Char = 'A'
+    val typeString: String = "ABCD"
+    val typesInKotlin = 9
+
+    val newTypeShort = typeByte.toShort()
+    val newTypeInt = typeShort.toInt()
+    val newTypeLong = typeInt.toLong()
+    val newTypeFloat = typeLong.toFloat()
+    val newTypeDouble = typeFloat.toDouble()
+    val newTypeByte = typeDouble.toInt().toByte()
+
+    println(
+        "There are $typesInKotlin types of variables in Kotlin:" +
+                "\n$newTypeByte" +
+                "\n$newTypeShort" +
+                "\n$newTypeInt" +
+                "\n$newTypeLong" +
+                "\n$newTypeFloat" +
+                "\n$newTypeDouble" +
+                "\n$typeBoolean" +
+                "\n$typeChar" +
+                "\n$typeString"
+    )
+}
