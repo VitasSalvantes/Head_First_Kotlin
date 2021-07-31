34a5f1b953db01e4ba11903f8f8cbd53d95b351d
VitasSalvantes <salvantesvitas@gmail.com>
2021-07-31 14:03:18 +0200

    Added function to get the user's choice

--- src/main/kotlin/chapter_3/rock_paper_scissors/Game.kt
+++ src/main/kotlin/chapter_3/rock_paper_scissors/Game.kt
@@ -5,6 +5,29 @@ import kotlin.random.Random
 fun main() {
     val options = arrayOf("Rock", "Paper", "Scissors")
     val gameChoice = getGameChoice(options)
+    val userChoice = getUserChoice(options)
 }
 
 fun getGameChoice(options: Array<String>) = options[Random.nextInt(until = options.size)]
+
+fun getUserChoice(options: Array<String>): String {
+    var isValidChoice = false
+    var userChoice = ""
+
+    while (!isValidChoice) {
+        println("Please enter one of the following:")
+        for (item in options) print(" $item")
+        println(".")
+
+        val userInput = readLine()
+
+        if (userInput != null && userInput in options) {
+            isValidChoice = true
+            userChoice = userInput
+        }
+
+        if (!isValidChoice) println("You must enter a valid choice!\n")
+    }
+
+    return userChoice
+}
