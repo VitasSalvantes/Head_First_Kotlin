709e6e4e56c4820ba19a7f046e09c926210449e8
VitasSalvantes <salvantesvitas@gmail.com>
2021-08-13 16:32:46 +0200

    Added code with let

--- src/main/kotlin/chapter_8/SuperNull.kt
+++ src/main/kotlin/chapter_8/SuperNull.kt
@@ -1,6 +1,6 @@
 package chapter_8
 
-class Dog(val name: String,var age: Int)
+class Dog(val name: String, var age: Int)
 
 fun main() {
     var bob: Dog? = Dog("Bob", 12)
@@ -9,6 +9,18 @@ fun main() {
     bob?.age = 2
     println(bob?.age)
 
+    var c = bob!!.age
+
+    bob?.let {
+        println(it.age)
+    }
+
+    println(bob?.age ?: 1)
+
     bob = null
     println(bob?.age)
+
+    bob?.let {
+        println("Bob. Just Bob.")
+    }
 }
