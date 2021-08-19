9e58b8fb68ae796b49639c5dcf47d21935be5198
VitasSalvantes <salvantesvitas@gmail.com>
2021-08-19 18:14:38 +0200

    Learned in and out in generics

--- src/main/kotlin/chapter_10/Pets.kt
+++ src/main/kotlin/chapter_10/Pets.kt
@@ -2,13 +2,13 @@ package chapter_10
 
 abstract class Pet(var name: String)
 
-class dog(name: String) : Pet(name)
+class Dog(name: String) : Pet(name)
 
-class cat(name: String) : Pet(name)
+class Cat(name: String) : Pet(name)
 
-class fish(name: String) : Pet(name)
+class Fish(name: String) : Pet(name)
 
-class contest<T : Pet> {
+class Contest<T : Pet>(var vet: Vet<in T>) {
 
     val scores: MutableMap<T, Int> = mutableMapOf()
 
@@ -25,4 +25,68 @@ class contest<T : Pet> {
         }
         return winners
     }
+}
+
+interface Retailer<out T> {
+    fun sell(): T
+}
+
+class CatRetailer : Retailer<Cat> {
+    override fun sell(): Cat {
+        println("Sell cat")
+        return Cat("")
+    }
+}
+
+class FishRetailer : Retailer<Fish> {
+    override fun sell(): Fish {
+        println("Sell fish")
+        return Fish("")
+    }
+}
+
+class DogRetailer : Retailer<Dog> {
+    override fun sell(): Dog {
+        println("Sell dog")
+        return Dog("")
+    }
+}
+
+class Vet<T : Pet> {
+    fun treat(t: T) {
+        println("Treat Pet ${t.name}")
+    }
+}
+
+fun main() {
+    val catFuzz = Cat("Fuzz Lightyear")
+    val catKatsu = Cat("Katsu")
+    val fishFinny = Fish("Finny McGraw")
+
+    val catVet = Vet<Cat>()
+    val fishVet = Vet<Fish>()
+    val petVet = Vet<Pet>()
+    catVet.treat(catFuzz)
+    petVet.treat(catKatsu)
+    petVet.treat(fishFinny)
+
+    val catContest = Contest<Cat>(catVet)
+    catContest.addScore(catFuzz, 50)
+    catContest.addScore(catKatsu, 45)
+    val topCat = catContest.getWinners().first()
+    println("Cat contest winner is ${topCat.name}")
+
+    val petContest = Contest<Pet>(petVet)
+    petContest.addScore(fishFinny, 99)
+    petContest.addScore(catFuzz, 50)
+    petContest.addScore(catKatsu, 45)
+    val topPet = petContest.getWinners().first()
+    println("Pet contest winner is ${topPet.name}")
+
+    val fishContest = Contest<Fish>(petVet)
+
+    val dogRetailer: Retailer<Dog> = DogRetailer()
+    val catRetailer: Retailer<Cat> = CatRetailer()
+    val petRetailer: Retailer<Pet> = CatRetailer()
+    petRetailer.sell()
 }
