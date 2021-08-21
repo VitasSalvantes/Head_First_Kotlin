d8b82539b86c4cac761ea35afc89617afa39c826
VitasSalvantes <salvantesvitas@gmail.com>
2021-08-21 16:06:35 +0200

    Learned more lambdas

--- src/main/kotlin/chapter_11/MyLambdas.kt
+++ src/main/kotlin/chapter_11/MyLambdas.kt
@@ -16,4 +16,19 @@ fun main() {
     println(c(90))
     println(d(18))
 
+    convert(12.0) { cc: Double -> cc * 1.8 + 32 }
+    convertFive { dd: Int -> dd * 1.8 + 32 }
+    convertFive { it * 1.8 + 32 }
+}
+
+fun convert(x: Double, converter: (Double) -> Double): Double {
+    val result = converter(x)
+    println("$x is converted to $result")
+    return result
+}
+
+fun convertFive(converter: (Int) -> Double): Double {
+    val result = converter(5)
+    println("5 is converted to $result")
+    return result
 }
