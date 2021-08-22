7953248e7fe414d2ba8d2f82d78b8148bf5d1362
VitasSalvantes <salvantesvitas@gmail.com>
2021-08-22 16:00:11 +0200

    Learned more lambdas

--- src/main/kotlin/chapter_11/MyLambdas.kt
+++ src/main/kotlin/chapter_11/MyLambdas.kt
@@ -1,5 +1,7 @@
 package chapter_11
 
+typealias DoubleConversion = (Double) -> Double
+
 fun main() {
     val a = { "Pow!" }
     val b = { x: Int -> x + 5 }
@@ -19,6 +21,16 @@ fun main() {
     convert(12.0) { cc: Double -> cc * 1.8 + 32 }
     convertFive { dd: Int -> dd * 1.8 + 32 }
     convertFive { it * 1.8 + 32 }
+
+    val lambda1 = getConversionLambda("KgsToPounds")
+    val convertedValue = getConversionLambda("CentigradeToFahrenheit")(20.0)
+    println(convertedValue)
+
+    val kgsToPounds: DoubleConversion = {it * 2.204623}
+    val poundsToUSTons: DoubleConversion = {it / 2000.0}
+    val kgsToUSTons = combine(kgsToPounds, poundsToUSTons)
+    val usTons = kgsToUSTons(1000.0)
+    println(usTons)
 }
 
 fun convert(x: Double, converter: (Double) -> Double): Double {
@@ -31,4 +43,25 @@ fun convertFive(converter: (Int) -> Double): Double {
     val result = converter(5)
     println("5 is converted to $result")
     return result
+}
+
+fun getConversionLambda(str: String): (Double) -> Double {
+    when (str) {
+        "CentigradeToFahrenheit" -> {
+            return { it * 1.8 + 32 }
+        }
+        "KgsToPounds" -> {
+            return { it * 2.204623 }
+        }
+        "PoundsToUSTons" -> {
+            return { it / 2000.0 }
+        }
+        else -> {
+            return { it }
+        }
+    }
+}
+
+fun combine(lambda1: DoubleConversion, lambda2: DoubleConversion): DoubleConversion {
+    return {x -> lambda2(lambda1(x))}
 }
