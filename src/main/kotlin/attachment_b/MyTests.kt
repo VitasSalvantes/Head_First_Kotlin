e6b36e826ec5c97c04cd4905e8d1be9966dbadc6
VitasSalvantes <salvantesvitas@gmail.com>
2021-08-27 17:49:56 +0200

    Learned tests

---
+++ src/main/kotlin/attachment_b/MyTests.kt
@@ -0,0 +1,8 @@
+package attachment_b
+
+class Totaller(var total: Int = 0) {
+    fun add(num: Int): Int {
+        total += num
+        return total
+    }
+}
---
+++ src/test/kotlin/attachment_b/TotallerTest.kt
@@ -0,0 +1,15 @@
+package attachment_b
+
+import org.junit.Test
+import org.junit.jupiter.api.Assertions.*
+
+class TotallerTest {
+
+    @Test
+    fun `should be able to add 3 and 4`() {
+        val totaller = Totaller()
+        assertEquals(3, totaller.add(3))
+        assertEquals(7, totaller.add(4))
+        assertEquals(7, totaller.total)
+    }
+}
